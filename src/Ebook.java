ec5da7e193bdfffbf4e520aa1b7c1c48f0bece90
Jiabei1208 <51316334+Jiabei1208@users.noreply.github.com>
2019-11-06 16:27:29 -0500

    jiabei commit

--- src/Ebook.java
+++ src/Ebook.java
@@ -1,9 +1,9 @@
 
 public class Ebook extends Media {
-	public Ebook(long Id,String Title,String Year, String Genre,String ISBN,String Pulisher,String Author,long numberOfCopy,
+	public Ebook(String Title,String Year, String Genre,String ISBN,String Pulisher,String Author,long numberOfCopy,
 			boolean isNewArrive,long Maxrent) 
 	{
-		super(Id,Title,Year,Genre,ISBN,Pulisher,Author,numberOfCopy,isNewArrive,Maxrent);
+		super(Title,Year,Genre,ISBN,Pulisher,Author,numberOfCopy,isNewArrive,Maxrent);
 			
 	}
 	public void setMaxrent(long Maxrent)
