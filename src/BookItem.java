04b932111550c1173d24b1d8c1715df651450edb
Jiabei1208 <51316334+Jiabei1208@users.noreply.github.com>
2019-11-06 13:10:20 -0500

    Jiabei commit

---
+++ src/BookItem.java
@@ -0,0 +1,13 @@
+
+public class BookItem extends Media {
+	public BookItem(long Id,String Title,String Year, String Genre,String ISBN,String Pulisher,String Author,
+			long numberOfCopy,boolean isNewArrive,long Maxrent) {
+		super(Id,Title,Year,Genre,ISBN,Pulisher,Author,numberOfCopy,isNewArrive,Maxrent);
+			
+	}
+	public void setMaxrent(long Maxrent)
+	{
+		Maxrent = 30;
+	}
+	
+}
