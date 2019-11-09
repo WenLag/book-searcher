249041734cd85140584011582949229d8d6294fa
Jiabei1208 <51316334+Jiabei1208@users.noreply.github.com>
2019-11-09 17:58:00 -0500

    Jiabei

--- src/Magazine.java
+++ src/Magazine.java
@@ -1,14 +1,14 @@
 
 public class Magazine extends Media {
-	public Magazine(String Title,String Year, String Genre,String ISBN,String Pulisher,
+	public Magazine(String Title,String Year, String Genre,long Rating,String ISBN,String Pulisher,
 			String Author,long numberOfCopy,boolean isNewArrive,long Maxrent) {
-		super(Title,Year,Genre,ISBN,Pulisher,Author,numberOfCopy,isNewArrive,Maxrent);
+		super(Title,Year,Genre,Rating,ISBN,Pulisher,Author,numberOfCopy,isNewArrive,Maxrent);
 			
 	}
 	public String toString()
 	{
 		
-		return "title:"+this.Title+"year:"+this.Year+"genre:"+this.Genre+"ISBN:"+this.ISBN
+		return "title:"+this.Title+"year:"+this.Year+"genre:"+this.Genre+"rating:"+this.Rating+this.Genre+"ISBN:"+this.ISBN
 				+"publisher:"+this.Publisher+"author:"+this.Author+"number of copy:"+this.numberOfCopy
 				+"is new arrvial:"+this.isNewArrive();
 	}
